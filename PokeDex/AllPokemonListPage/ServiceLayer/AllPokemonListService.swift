ba0cc8c1a5dd577a00d43ff9e96afae7464187b1
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-04-17 15:52:39 +0800

    Back to main thread after receiving response in `AllPokemonListService`

--- PokeDex/AllPokemonListPage/ServiceLayer/AllPokemonListService.swift
+++ PokeDex/AllPokemonListPage/ServiceLayer/AllPokemonListService.swift
@@ -16,19 +16,21 @@ class AllPokemonListService {
     let client = AllPokemonListHTTPClient()
     func loadAllPokemonList(completion: @escaping (Result<AllPokemonList, AllPokemonListServiceError>) -> Void) {
         client.reuestAllPokemonList { result in
-            switch result {
-            case let .success((data, reponse)): // tuple (Data, HTTPURLResponse) -> type
-                // do success behavior
-                do {
-                    let allPokemonListDTO = try JSONDecoder().decode(AllPokemonListDTO.self, from: data)
-                    let allPokemonList = AllPokemonList(from: allPokemonListDTO)
-                    completion(.success(allPokemonList))
-                } catch {
-                    completion(.failure(.JSONParsingError))
+            DispatchQueue.main.async {
+                switch result {
+                case let .success((data, reponse)): // tuple (Data, HTTPURLResponse) -> type
+                    // do success behavior
+                    do {
+                        let allPokemonListDTO = try JSONDecoder().decode(AllPokemonListDTO.self, from: data)
+                        let allPokemonList = AllPokemonList(from: allPokemonListDTO)
+                        completion(.success(allPokemonList))
+                    } catch {
+                        completion(.failure(.JSONParsingError))
+                    }
+                case .failure:
+                    // do failure behavior
+                    completion(.failure(.NetworkError))
                 }
-            case .failure:
-                // do failure behavior
-                completion(.failure(.NetworkError))
             }
         }
     }
