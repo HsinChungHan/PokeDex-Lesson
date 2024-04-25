8eb39e7d95382f301a4bf319d0c2c26a9cf67e51
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-04-25 09:10:13 +0800

    Present `allPokemonListViewController` view in `ViewController`

--- PokeDex/ViewController.swift
+++ PokeDex/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let allPokemonListViewController = AllPokemonListViewController.init()
+    let allPokemonListViewModel = AllPokemonListViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        allPokemonListViewModel.delegate = self
+//        allPokemonListViewModel.loadAllPokemonList()
+        
+        view.addSubview(allPokemonListViewController.view)
+        allPokemonListViewController.view.fillWithPadding()
     }
 
 
 }
 
+extension ViewController: AllPokemonListViewModelDelegate {
+    func allPokemonListViewModel(_ allPokemonListViewModel: AllPokemonListViewModel, allPokemonListDidUpdate allPokemonList: AllPokemonList) {
+        print(allPokemonList)
+    }
+    
+    func allPokemonListViewModel(_ allPokemonListViewModel: AllPokemonListViewModel, allPokemonListErrorDidUpdate error: AllPokemonListServiceError) {
+        print(error)
+    }
+}
