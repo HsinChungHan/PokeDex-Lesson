7b512d641de8086114b9b7a6258253be2409da20
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-04-25 09:25:45 +0800

    Implement `makePokemonInfoRequest` and  `requestPokemonInfo` in `PokemonInfoHTTPClient`

--- PokeDex/PokemonInfoHTTPClient.swift
+++ PokeDex/PokemonInfoHTTPClient.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 class PokemonInfoHTTPClient: HTTPClient {
+    func requestPokemonInfo(with name: String, completion: @escaping (Result<(Data, HTTPURLResponse), HTTPClientError>) -> Void) {
+            let requestType = makePokemonInfoRequest(with: name)
+            request(with: requestType, completion: completion)
+        }
+    
     func reuestPokemonImage(with id: String, completion: @escaping (Result<(Data, HTTPURLResponse), HTTPClientError>) -> Void) {
         let requestType = makePokemonImageRequest(with: id)
         request(with: requestType, completion: completion)
@@ -25,6 +30,13 @@ class PokemonInfoHTTPClient: HTTPClient {
      }
  */
 fileprivate extension PokemonInfoHTTPClient {
+    // https://pokeapi.co/api/v2/pokemon/{name}
+    func makePokemonInfoRequest(with name: String) -> RequestType {
+        let url = URL.init(string: "https://pokeapi.co/api/v2")!
+        let path = "/pokemon/\(name)"
+        return RequestType(httpMethod: .GET, domainURL: url, path: path)
+    }
+    
     var headers: [String : String] {
         [
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"
