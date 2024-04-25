78e1270099f51fc6f9f2f137c059acf9ead5b2b5
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-04-25 09:08:02 +0800

    Add `headers` in `RequestType`

--- PokeDex/NetworkModule/RequestType.swift
+++ PokeDex/NetworkModule/RequestType.swift
@@ -8,44 +8,54 @@
 import Foundation
 
 enum HttpMethod: String {
-  case GET = "GET"
-  case POST = "POST"
+    case GET = "GET"
+    case POST = "POST"
 }
 
 struct RequestType {
-  let httpMethod: HttpMethod
-  let domainURL: URL
-  let path: String
-  let queryItems: [URLQueryItem]
-  let body: Data?
-   
-  init(httpMethod: HttpMethod, domainURL: URL, path: String, queryItems: [URLQueryItem], body: Data? = nil) {
-    self.httpMethod = httpMethod
-    self.domainURL = domainURL
-    self.path = path
-    self.queryItems = queryItems
-    self.body = body
-  }
-   
-  func getFullURL() -> URL {
-    var urlComponents = URLComponents(url: domainURL, resolvingAgainstBaseURL: false)
-    urlComponents?.path += path
-    if !queryItems.isEmpty {
-      urlComponents?.queryItems = queryItems
+    let httpMethod: HttpMethod
+    let domainURL: URL
+    let path: String
+    let queryItems: [URLQueryItem]
+    let body: Data?
+    let headers: [String: String]?
+    
+    
+    init(httpMethod: HttpMethod, domainURL: URL, path: String, queryItems: [URLQueryItem] = [], body: Data? = nil, headers: [String: String]? = nil) {
+        self.httpMethod = httpMethod
+        self.domainURL = domainURL
+        self.path = path
+        self.queryItems = queryItems
+        self.body = body
+        self.headers = headers
     }
-    guard let url = urlComponents?.url else {
-      fatalError("URL should not be nil!")
+    
+    func getFullURL() -> URL {
+        var urlComponents = URLComponents(url: domainURL, resolvingAgainstBaseURL: false)
+        urlComponents?.path += path
+        if !queryItems.isEmpty {
+            urlComponents?.queryItems = queryItems
+        }
+        guard let url = urlComponents?.url else {
+            fatalError("URL should not be nil!")
+        }
+        return url
     }
-    return url
-  }
-   
-  func getURLRequest() -> URLRequest {
-    let fullURL = getFullURL()
-    var request = URLRequest(url: fullURL)
-    request.httpMethod = httpMethod.rawValue
-    if let body = body {
-      request.httpBody = body
+    
+    func getURLRequest() -> URLRequest {
+        let fullURL = getFullURL()
+        var request = URLRequest(url: fullURL)
+        request.httpMethod = httpMethod.rawValue
+        if let body = body {
+            request.httpBody = body
+        }
+        
+        if let headers = headers {
+            for (headerField, headerValue) in headers {
+                request.setValue(headerValue, forHTTPHeaderField: headerField)
+            }
+        }
+        
+        return request
     }
-    return request
-  }
 }
